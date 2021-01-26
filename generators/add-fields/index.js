bac7051dba54d52db4673a760cef193fc4842845
kirilleropkin <krill221@gmail.com>
2021-01-26 20:38:51 +0300

    bugs update

--- generators/add-fields/index.js
+++ generators/add-fields/index.js
@@ -50,8 +50,8 @@ module.exports = class extends Generator {
     var resNewTextNew = `new ${this.answers.model}({\n\t\t\t\t\t\t${this.answers.fields.map(f => `${f[0]}`).join(',\n\t\t\t\t\t\t')},`;
     var awaitText = `item = await ${this.answers.model}.findById\\(id\\);`;
     var awaitTextNew = `item = await ${this.answers.model}.findById(id);\n\t\t\t\t\t${this.answers.fields.map(f => `if (${f[0]} !== undefined) item.${f[0]} = ${f[0]};`).join('\n\t\t\t\t\t')}`;
-    var asyncUpdateText = `async update${this.answers.model}\\(_, { `;
-    var asyncUpdateTextNew = `async update${this.answers.model}(_, { ${this.answers.fields.map(f => f[0]).join(', ')}, `;
+    var asyncUpdateText = `async update${this.answers.model}\\(_, { input: { `;
+    var asyncUpdateTextNew = `async update${this.answers.model}(_, { input: { ${this.answers.fields.map(f => f[0]).join(', ')}, `;
     ResolversFile = ResolversFile.toString().replace(new RegExp(resNewText, 'g'), resNewTextNew);
     ResolversFile = ResolversFile.toString().replace(new RegExp(awaitText, 'g'), awaitTextNew);
     ResolversFile = ResolversFile.toString().replace(new RegExp(asyncUpdateText, 'g'), asyncUpdateTextNew);

099dffe720b59802a5654b6ef1aa957b938c5ac2
kirilleropkin <krill221@gmail.com>
2021-04-08 21:27:17 +0300

    change has-many

--- generators/app/templates/ins.sh
+++ generators/app/templates/ins.sh
@@ -7,9 +7,10 @@
 #yarn delete-has-estime Post Like
 
 # 1. TEXT CHAT (with chat rooms and messages)
-#yarn add-model Room name:String
+#yarn add-model Chat name:String
 #yarn add-model Message text:String
-#yarn add-has-many Room Message
+#yarn add-has-many Chat Message
+#yarn add-has-many User Message
 
 # 2. LIKES (add to model)
 #yarn add-model Like value:String userId:ID

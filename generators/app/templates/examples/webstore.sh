bf87e884e3eba2bf69cef2401c3409f4446792e6
kirilleropkin <krill221@gmail.com>
2021-04-14 21:36:48 +0300

    change query look

--- generators/app/templates/examples/webstore.sh
+++ generators/app/templates/examples/webstore.sh
@@ -7,3 +7,15 @@ yarn add-has-many Product OrderItem
 yarn add-has-estime Order OrderItem
 yarn add-has-many User OrderItem
 yarn add-has-many User Order
+
+# move webstore-pages to pages
+
+#add to App.js
+#<PublicMainLayout path="/catalog" component={Catalog} /> 
+#<PrivateMainLayout path="/cart" component={Cart} /> 
+#<PrivateMainLayout path="/delivery" component={Delivery} /> 
+
+#add to Menu.js
+# catalog
+# cart
+# delivery
